89e3236ee37dde47f4880e000a0ac1c27d0f0ccf
13101989 <valentin.damache@outlook.com>
2024-03-20 18:10:38 +0200

    work on deployment in aws with terraform

---
+++ terraform/main.tf
@@ -0,0 +1,53 @@
+# Configure Terraform settings
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+  }
+
+  cloud {
+    organization = "my-skillab"
+
+    workspaces {
+      name = "chatbot"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = var.region
+}
+
+
+resource "kubernetes_secret" "my_secret" {
+  metadata {
+    name      = "my-secret"
+    namespace = var.namespace
+  }
+
+  data = {
+    OPENAI_KEY        = var.REST_CLIENT_SECRET
+    POSTGRES_DB       = var.POSTGRES_DB
+    POSTGRES_USER     = var.POSTGRES_USER
+    POSTGRES_PASSWORD = var.POSTGRES_PASSWORD
+    POSTGRES_HOST     = var.POSTGRES_HOST
+  }
+}
+
+resource "helm_release" "my_chatbot_release" {
+  name    = "my-chatbot-release"
+  chart   = "${path.module}/../k8s"
+  version = "0.0.1"
+
+  namespace = var.namespace
+
+  values = [file("${path.module}/values.yaml")]
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,36 @@
+# Define AWS access key ID variable
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS access key ID"
+}
+
+# Define AWS secret access key variable
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS secret access key"
+}
+
+# Define secrets variables
+variable "REST_CLIENT_SECRET" {
+  description = "OPENAI authentication secret"
+}
+variable "POSTGRES_DB" {
+  description = "Postgres database name"
+}
+variable "POSTGRES_USER" {
+  description = "Postgres login user"
+}
+variable "POSTGRES_PASSWORD" {
+  description = "Postgress login password"
+}
+variable "POSTGRES_HOST" {
+  description = "ost of postgres database"
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "namespace" {
+  description = "Kubernetes namespace"
+  type        = string
+}
