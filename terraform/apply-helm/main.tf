3a6445b74b6e601d141e666cff38b7ab3fa44034
13101989 <valentin.damache@outlook.com>
2024-03-21 17:53:11 +0200

    work in eks cluster deployment

--- terraform/apply-helm/main.tf
+++ terraform/apply-helm/main.tf
@@ -5,6 +5,16 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.8.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.17.0"
+    }
   }
 
   cloud {
@@ -21,27 +31,31 @@ provider "aws" {
   region = var.region
 }
 
+data "terraform_remote_state" "eks" {
+  backend = "local"
+  config = {
+    path = "../deploy-eks/.terraform/terraform.tfstate"
+  }
+}
+
 data "aws_eks_cluster" "cluster" {
-  name = var.cluster_name
+  name = data.terraform_remote_state.eks.outputs.cluster_name
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = var.cluster_name
+  name = data.terraform_remote_state.eks.outputs.cluster_name
 }
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+    command     = "aws"
   }
 }
+
 
 resource "kubernetes_secret" "my_secret" {
   metadata {
@@ -57,6 +71,20 @@ resource "kubernetes_secret" "my_secret" {
     POSTGRES_HOST     = var.POSTGRES_HOST
   }
 }
+
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+      command     = "aws"
+    }
+  }
+}
+
 
 resource "helm_release" "chatbot_release" {
   name  = "chatbot-release"
