537b7f70e7cde74c62152a0dcb417dea189f1ea9
13101989 <valentin.damache@outlook.com>
2024-03-21 15:15:21 +0200

    check new token

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -22,8 +22,3 @@ def test_healthcheck_data():
     assert response.status_code == 200
     assert response.json() == {"message": "Chatbot is healthy"}
 
-
-def test_get_version():
-    response = client.get("/version")
-    assert response.status_code == 200
-    assert response.json() == "version: 1.1.1"
